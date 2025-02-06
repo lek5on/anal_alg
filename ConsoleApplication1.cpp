5a629c04bf89321f997d934204992e076c609276
lek5on <lek5on@yandex.ru>
2025-02-06 13:40:40 +0300

    Update ConsoleApplication1.cpp

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -18,8 +18,8 @@ int main() {
     int* results = new int[size];
     int index = 0;
     for (int n = 2; n <= max_n; n += step) {
-        int** matrix = new int* [n];                //ñîçäàíèå ìàòðèöû è å¸ çàïîëíåíèå
-        int* vector = new int[n];                  //ñîçäàíèå âåêòîðà è åãî çàïîëíåíèå
+        int** matrix = new int* [n];               
+        int* vector = new int[n];                 
         int operations = 0;
         for (int i = 0; i < n; i++) {
             matrix[i] = new int[n];
@@ -37,7 +37,7 @@ int main() {
             operations++;
         }
 
-        for (int i = 0; i < n; i++) {               //ïåðåìíîæàåì ýëåìåíòû âåêòîðà íà ýëåìåíòû ìàòðèöû è ïîëó÷àåì íîâûå çíà÷åíèÿ â ìàòðèöå
+        for (int i = 0; i < n; i++) {               
             for (int j = 0; j < n; j++) {
                 matrix[i][j] *= vector[i];
                 operations+=2;
@@ -45,11 +45,11 @@ int main() {
         }
         for (int i = 0; i < n; i++)
         {
-            vector[i] = 0;  //îáíóëÿåì ýëåìåíòû âåêòîðà äëÿ çàïèñè íîâûõ îòâåòîâ
+            vector[i] = 0;  
         }
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                vector[i] += matrix[i][j];         //çàïèñûâàåì â âåêòîð ñóììó çíà÷åíèé ñòðîêè, êîòîðûå ïîëó÷èëèñü â ìàòðèöå
+                vector[i] += matrix[i][j];         
                 operations+=2;
             }
         }
