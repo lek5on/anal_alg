f477e28be7fbb0a1445fb56726931165c29a646b
lek5on <lek5on@yandex.ru>
2025-02-06 13:41:10 +0300

    Update ConsoleApplication1.cpp

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -9,7 +9,7 @@ int main() {
     ofstream outfile("result.txt");
     if (!outfile)
     {
-        cerr << "îøèáêà îòêðûòèÿ ôàéëà" << endl;
+        cerr << oshibka pri otritie faila" << endl;
         return 1;
     }
     int max_n = 30;
